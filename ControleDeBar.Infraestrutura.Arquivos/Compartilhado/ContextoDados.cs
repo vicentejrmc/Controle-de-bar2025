c1132a00c28abe45adb98f802b8ea88deadc2c9e
vicentejrmc <vicente_pc@outlook.com>
2025-06-11 14:27:59 -0300

    Implementada Infraestrutura

---
+++ ControleDeBar.Infraestrutura.Arquivos/Compartilhado/ContextoDados.cs
@@ -0,0 +1,65 @@
+﻿using System.Text.Json.Serialization;
+using System.Text.Json;
+using controleDeBar.Dominio.ModuloMesa;
+
+
+namespace ControleDeBar.Infraestrura.Arquivos.Compartilhado;
+
+public class ContextoDados
+{
+    private string pastaArmazenamento = "C:\\temp";
+    private string arquivoArmazenamento = "dados-controle-bar.json";
+
+    public List<Mesa> Mesas { get; set; }
+
+    public ContextoDados()
+    {
+        Mesas = new List<Mesa>();
+
+    }
+    public ContextoDados(bool carregarDados) : this()
+    {
+        if (carregarDados)
+            Carregar();
+    }
+
+    public void Salvar()
+    {
+        string caminhoCompleto = Path.Combine(pastaArmazenamento, arquivoArmazenamento);
+
+        JsonSerializerOptions jsonOptions = new JsonSerializerOptions();
+        jsonOptions.WriteIndented = true;
+        jsonOptions.ReferenceHandler = ReferenceHandler.Preserve;
+
+        string json = JsonSerializer.Serialize(this, jsonOptions);
+
+        if (!Directory.Exists(pastaArmazenamento))
+            Directory.CreateDirectory(pastaArmazenamento);
+
+        File.WriteAllText(caminhoCompleto, json);
+    }
+
+    public void Carregar()
+    {
+        string caminhoCompleto = Path.Combine(pastaArmazenamento, arquivoArmazenamento);
+
+        if (!File.Exists(caminhoCompleto)) return;
+
+        string json = File.ReadAllText(caminhoCompleto);
+
+        if (string.IsNullOrWhiteSpace(json)) return;
+
+        JsonSerializerOptions jsonOptions = new JsonSerializerOptions();
+        jsonOptions.ReferenceHandler = ReferenceHandler.Preserve;
+
+        ContextoDados contextoArmazenado = JsonSerializer.Deserialize<ContextoDados>(
+            json, 
+            jsonOptions
+        )!;
+
+        if (contextoArmazenado == null) return;
+
+        Mesas = contextoArmazenado.Mesas;
+
+    }
+}
---
+++ ControleDeBar.Infraestrutura.Arquivos/ModuloMesa/RepositorioMesaEmArquivo.cs
@@ -0,0 +1,15 @@
+﻿using controleDeBar.Dominio.ModuloMesa;
+using ControleDeBar.Infraestrura.Arquivos.Compartilhado;
+
+namespace ControleDeBar.Infraestrutura.Arquivos.ModuloMesa
+{
+    public class RepositorioMesaEmArquivo : RepositorioBaseEmArquivo<Mesa>, IRepositorioMesa
+    {
+        public RepositorioMesaEmArquivo(ContextoDados contexto) : base(contexto) { }
+
+        protected override List<Mesa> ObterRegistros()
+        {
+            return contexto.Mesas;
+        }
+    }
+}
