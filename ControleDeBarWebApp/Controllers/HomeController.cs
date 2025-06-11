f844566b2241a6f1ddd4f67d9a7d40686208c766
vicentejrmc <vicente_pc@outlook.com>
2025-06-11 10:13:47 -0300

    Implementada configuração inicial do projeto

---
+++ ControleDeBarWebApp/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeBarWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ControleDeBarWebApp/Program.cs
@@ -0,0 +1,18 @@
+namespace ControleDeBarWebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            var app = builder.Build();
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.MapDefaultControllerRoute();
+
+            app.Run();
+        }
+    }
+}
