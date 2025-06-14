47e177418e05d3485320b07c2d8771862d487329
vicentejrmc <vicente_pc@outlook.com>
2025-06-14 10:04:10 -0300

    Implementado ProdutoController/ View Index(visualizar)

---
+++ ControleDeBarWebApp/Controllers/ProdutoController.cs
@@ -0,0 +1,31 @@
+﻿using controleDeBar.Dominio.ModuloProduto;
+using ControleDeBar.Infraestrura.Arquivos.Compartilhado;
+using ControleDeBar.Infraestrutura.Arquivos.ModuloProduto;
+using ControleDeBarWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeBarWebApp.Controllers
+{
+    [Route("produtos")]
+    public class ProdutoController : Controller
+    {
+        private readonly ContextoDados contextoDados;
+        private readonly IRepositorioProduto repositorioProduto;
+
+        public ProdutoController()
+        {
+            contextoDados = new ContextoDados(true);
+            repositorioProduto = new RepositorioProdutoEmArquivo(contextoDados);
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+           var registros = repositorioProduto.SelecionarRegistros();
+
+            var visualizarVM = new VisualizarProdutosViewModel(registros);
+
+            return View(visualizarVM);
+        }
+    }
+}
