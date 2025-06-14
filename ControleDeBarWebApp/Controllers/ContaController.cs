fe89e1c624fa78c6959191a70067844cc8d267af
vicentejrmc <vicente_pc@outlook.com>
2025-06-14 14:50:06 -0300

    implementado importes ContaController

--- ControleDeBarWebApp/Controllers/ContaController.cs
+++ ControleDeBarWebApp/Controllers/ContaController.cs
@@ -3,10 +3,15 @@ using controleDeBar.Dominio.ModuloGarcom;
 using controleDeBar.Dominio.ModuloMesa;
 using controleDeBar.Dominio.ModuloProduto;
 using ControleDeBar.Infraestrura.Arquivos.Compartilhado;
+using ControleDeBar.Infraestrutura.Arquivos.ModuloConta;
+using ControleDeBar.Infraestrutura.Arquivos.ModuloGarcom;
+using ControleDeBar.Infraestrutura.Arquivos.ModuloMesa;
+using ControleDeBar.Infraestrutura.Arquivos.ModuloProduto;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ControleDeBarWebApp.Controllers
 {
+    [Route("contas")]
     public class ContaController : Controller
     {
         private readonly ContextoDados contextoDados;
@@ -15,9 +20,17 @@ namespace ControleDeBarWebApp.Controllers
         private readonly IRepositorioMesa repositorioMesa;
         private readonly IRepositorioProduto repositorioProduto;
 
+        public ContaController()
+        {
+            contextoDados = new ContextoDados(true);
+            repositorioConta = new RepositorioContaEmArquivo(contextoDados);
+            repositorioGarcom = new RepositorioGarcomEmArquivo(contextoDados);
+            repositorioMesa = new RepositorioMesaEmArquivo(contextoDados);
+            repositorioProduto = new RepositorioProdutoEmArquivo(contextoDados);
+        }
+
         public IActionResult Index()
         {
-
             return View();
         }
     }
