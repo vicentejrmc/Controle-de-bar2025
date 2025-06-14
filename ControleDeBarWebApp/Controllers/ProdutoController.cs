50981fe6ab9619392221cd79a1fde4ba86479cc5
vicentejrmc <vicente_pc@outlook.com>
2025-06-14 10:12:19 -0300

    Implementado Controller CadastrarProduto

--- ControleDeBarWebApp/Controllers/ProdutoController.cs
+++ ControleDeBarWebApp/Controllers/ProdutoController.cs
@@ -1,6 +1,7 @@
 ﻿using controleDeBar.Dominio.ModuloProduto;
 using ControleDeBar.Infraestrura.Arquivos.Compartilhado;
 using ControleDeBar.Infraestrutura.Arquivos.ModuloProduto;
+using ControleDeBarWebApp.Extensions;
 using ControleDeBarWebApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -26,6 +27,38 @@ namespace ControleDeBarWebApp.Controllers
             var visualizarVM = new VisualizarProdutosViewModel(registros);
 
             return View(visualizarVM);
+        }
+
+        [HttpGet("cadastrar")]
+        public IActionResult Cadastrar()
+        {
+            var cadastrarVM = new CadastrarProdutoViewModel();
+            return View(cadastrarVM);
+        }
+
+        [HttpPost("cadastrar")]
+        public IActionResult Cadastrar(CadastrarProdutoViewModel cadastrarVM)
+        {
+            var registros = repositorioProduto.SelecionarRegistros();
+
+            foreach (var produto in registros)
+            {
+                if (produto.Nome.Equals(cadastrarVM.Nome))
+                {
+                    ModelState.AddModelError("CadastroUnico", "Já existe um produto cadastrado com esse nome.");
+                    break;
+                }
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(cadastrarVM);
+            }
+
+            var novoProduto = cadastrarVM.ParaEntidade();
+            repositorioProduto.CadastrarRegistro(novoProduto);
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
