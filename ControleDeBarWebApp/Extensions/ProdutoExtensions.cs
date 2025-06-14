d5be8f5dd5adcb53c43495f8f01c106c4e0ad4a4
vicentejrmc <vicente_pc@outlook.com>
2025-06-14 09:54:51 -0300

    Implementado ProdutoExtensions / ProdutoViewModel com Validações

---
+++ ControleDeBarWebApp/Extensions/ProdutoExtensions.cs
@@ -0,0 +1,19 @@
+﻿using controleDeBar.Dominio.ModuloProduto;
+using ControleDeBarWebApp.Models;
+
+namespace ControleDeBarWebApp.Extensions
+{
+    public static class ProdutoExtensions
+    {
+        public static Produto ParaEntidade(this FormularioProdutoViewModel formularioVM)
+        {
+            return new Produto(formularioVM.Nome, formularioVM.Valor);
+        }
+
+        public static DetalhesProdutoViewModel ParaDetalhesVM(this Produto produto)
+        {
+            return new DetalhesProdutoViewModel(produto.Id, produto.Nome, produto.Valor);
+        }
+
+    }
+}
---
+++ ControleDeBarWebApp/Models/ProdutoViewModel.cs
@@ -0,0 +1,95 @@
+﻿using controleDeBar.Dominio.ModuloProduto;
+using ControleDeBarWebApp.Extensions;
+using System.ComponentModel.DataAnnotations;
+
+namespace ControleDeBarWebApp.Models
+{
+    public class FormularioProdutoViewModel
+    {
+        [Required(ErrorMessage = "O campo \"Nome\" é obrigatório")]
+        [MinLength(3, ErrorMessage = "O canmpo \"Nome\" precisa conter ao menos 3 caracteres")]
+        [MaxLength(50, ErrorMessage = "O campo \"Nome\" não pode conter mais de 50 caracteres")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo \"Valor\" é obrigatório")]
+        [DataType(DataType.Currency)]
+        [Range(0, double.MaxValue, ErrorMessage = "O campo \"Valor\" precisa conter um valor positivo.")]
+        public decimal Valor { get; set; }
+    }
+
+    public class CadastrarProdutoViewModel : FormularioProdutoViewModel
+    {
+        public CadastrarProdutoViewModel() { }
+
+        public CadastrarProdutoViewModel(string nome, decimal valor) : this()
+        {
+            Nome = nome;
+            Valor = valor;
+        }
+    }
+
+    public class EditarProdutoViewModel : FormularioProdutoViewModel
+    {
+        public Guid Id { get; set; }
+
+        public EditarProdutoViewModel() { }
+
+        public EditarProdutoViewModel(Guid id, string nome, decimal valor) : this()
+        {
+            Id = id;
+            Nome = nome;
+            Valor = valor;
+        }
+    }
+
+    public class ExcluirProdutoViewModel
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+
+        public ExcluirProdutoViewModel(Guid id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+    }
+
+    public class VisualizarProdutosViewModel
+    {
+        public List<DetalhesProdutoViewModel> Registros { get; set; }
+
+        public VisualizarProdutosViewModel(List<Produto> produtos)
+        {
+            Registros = new List<DetalhesProdutoViewModel>();
+
+            foreach (var p in produtos)
+                Registros.Add(p.ParaDetalhesVM());
+        }
+    }
+
+    public class DetalhesProdutoViewModel
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Valor { get; set; }
+
+        public DetalhesProdutoViewModel(Guid id, string nome, decimal valor)
+        {
+            Id = id;
+            Nome = nome;
+            Valor = valor;
+        }
+    }
+
+    public class SelecionarProdutoViewModel
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+
+        public SelecionarProdutoViewModel(Guid id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+    }
+}
