c1c617d4c01044e6516d261eb824f8023e0da1dd
Vicente Junior <vicente_pc@outlook.com>
2025-07-07 15:34:35 -0300

    Implementada Pasta Compatilhada - RepositorioBaseEmSql

---
+++ ControleDeBar.Infraestrutura.SqlServer/Compatilhado/IDBCommandExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Data;
+
+namespace ControleDeBar.Infraestrutura.SqlServer.Compatilhado
+{
+    public static class IDBCommandExtensions
+    {
+        public static void AdicionarParametro(this IDbCommand comando, string nome, object valor)
+        {
+            var parametro = comando.CreateParameter();
+            parametro.ParameterName = nome;
+            parametro.Value = valor;
+
+            comando.Parameters.Add(parametro);
+        }
+    }
+}
---
+++ ControleDeBar.Infraestrutura.SqlServer/Compatilhado/RepositorioBaseEmSql.cs
@@ -0,0 +1,116 @@
+﻿using controleDeBar.Dominio.Compatilhado;
+using System.Data;
+
+namespace ControleDeBar.Infraestrutura.SqlServer.Compatilhado
+{
+    public abstract class RepositorioBaseEmSql<T> where T : EntidadeBase<T>
+    {
+        protected abstract string SqlInserir { get; }
+        protected abstract string SqlEditar { get; }
+        protected abstract string SqlExcluir { get; }
+        protected abstract string SqlSelecionarPorId { get; }
+        protected abstract string SqlSelecionarTodos { get; }
+
+        protected readonly IDbConnection conexaoComBanco;
+
+        public RepositorioBaseEmSql(IDbConnection conexaoComBanco)
+        {
+            this.conexaoComBanco = conexaoComBanco;
+        }
+
+        public virtual void CadastrarRegistro(T novoRegistro)
+        {
+            var comandoInsercao = conexaoComBanco.CreateCommand();
+            comandoInsercao.CommandText = SqlInserir;
+
+            ConfigurarParametrosRegistro(novoRegistro, comandoInsercao);
+
+            conexaoComBanco.Open();
+
+            comandoInsercao.ExecuteNonQuery();
+
+            conexaoComBanco.Close();
+        }
+
+        public virtual bool EditarRegistro(Guid idRegistro, T registroEditado)
+        {
+            var comandoEdicao = conexaoComBanco.CreateCommand();
+            comandoEdicao.CommandText = SqlEditar;
+
+            registroEditado.Id = idRegistro;
+
+            ConfigurarParametrosRegistro(registroEditado, comandoEdicao);
+
+            conexaoComBanco.Open();
+
+            var linhasAfetadas = comandoEdicao.ExecuteNonQuery();
+
+            conexaoComBanco.Close();
+
+            return linhasAfetadas > 0;
+        }
+
+        public virtual bool ExcluirRegistro(Guid idRegistro)
+        {
+            var comandoExclusao = conexaoComBanco.CreateCommand();
+            comandoExclusao.CommandText = SqlExcluir;
+
+            comandoExclusao.AdicionarParametro("ID", idRegistro);
+
+            conexaoComBanco.Open();
+
+            var linhasAfetadas = comandoExclusao.ExecuteNonQuery();
+
+            conexaoComBanco.Close();
+
+            return linhasAfetadas > 0;
+        }
+
+        public virtual T? SelecionarRegistroPorId(Guid idRegistro)
+        {
+            var comandoSelecao = conexaoComBanco.CreateCommand();
+            comandoSelecao.CommandText = SqlSelecionarPorId;
+            comandoSelecao.AdicionarParametro("ID", idRegistro);
+
+            conexaoComBanco.Open();
+
+            var leitor = comandoSelecao.ExecuteReader();
+
+            T? registro = null;
+
+            if (leitor.Read())
+                registro = ConverterParaRegistro(leitor);
+
+            conexaoComBanco.Close();
+
+            return registro;
+        }
+
+        public virtual List<T> SelecionarRegistros()
+        {
+            var comandoSelecao = conexaoComBanco.CreateCommand();
+            comandoSelecao.CommandText = SqlSelecionarTodos;
+
+            conexaoComBanco.Open();
+
+            var leitor = comandoSelecao.ExecuteReader();
+
+            var registros = new List<T>();
+
+            while (leitor.Read())
+            {
+                var contato = ConverterParaRegistro(leitor);
+
+                registros.Add(contato);
+            }
+
+            conexaoComBanco.Close();
+
+            return registros;
+        }
+
+        protected abstract void ConfigurarParametrosRegistro(T registro, IDbCommand comando);
+
+        protected abstract T ConverterParaRegistro(IDataReader leitor);
+    }
+}
